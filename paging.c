f8d9ef8b54e7ed8527eb8812d4dbf8d4c6e481d0
Daksh Shah <daksh17336@iiitd.ac.in>
2019-03-31 05:06:27 +0530

    Implemented map_address, and in the process got mappages, walkpgdir to paging.c

--- paging.c
+++ paging.c
@@ -42,6 +42,68 @@ swap_page(pde_t *pgdir)
 	return 1;
 }
 
+// Return the address of the PTE in page table pgdir
+// that corresponds to virtual address va.  If alloc!=0,
+// create any required page table pages.
+static pte_t *
+walkpgdir(pde_t *pgdir, const void *va, int alloc)
+{
+  pde_t *pde;
+  pte_t *pgtab;
+
+  pde = &pgdir[PDX(va)];
+  if(*pde & PTE_P){
+    pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+  } else {
+    if(!alloc || (pgtab = (pte_t*)kalloc()) == 0)
+      return 0;
+    // Make sure all those PTE_P bits are zero.
+    memset(pgtab, 0, PGSIZE);
+    // The permissions here are overly generous, but they can
+    // be further restricted by the permissions in the page table
+    // entries, if necessary.
+    *pde = V2P(pgtab) | PTE_P | PTE_W | PTE_U;
+  }
+  return &pgtab[PTX(va)];
+}
+
+static pte_t *
+walkpgdirDONTCreate(pde_t *pgdir, const void *va)
+{
+  pde_t *pde;
+  pte_t *pgtab;
+
+  pde = &pgdir[PDX(va)];
+  if(*pde & PTE_P) pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+  else return 0;
+  return &pgtab[PTX(va)];
+}
+
+// Create PTEs for virtual addresses starting at va that refer to
+// physical addresses starting at pa. va and size might not
+// be page-aligned.
+static int
+mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm)
+{
+  char *a, *last;
+  pte_t *pte;
+
+  a = (char*)PGROUNDDOWN((uint)va);
+  last = (char*)PGROUNDDOWN(((uint)va) + size - 1);
+  for(;;){
+    if((pte = walkpgdir(pgdir, a, 1)) == 0)
+      return -1;
+    if(*pte & PTE_P)
+      panic("remap");
+    *pte = pa | perm | PTE_P;
+    if(a == last)
+      break;
+    a += PGSIZE;
+    pa += PGSIZE;
+  }
+  return 0;
+}
+
 /* Map a physical page to the virtual address addr.
  * If the page table entry points to a swapped block
  * restore the content of the page from the swapped
@@ -50,7 +112,33 @@ swap_page(pde_t *pgdir)
 void
 map_address(pde_t *pgdir, uint addr)
 {
-	panic("map_address is not implemented");
+    char* allocatedPage;
+    if( (allocatedPage = kalloc()) == 0 ){
+        cprintf("allocatedPage: %d\n",allocatedPage);
+        panic("You want me to kalloc a page in map_address huh?. Sorry I wont!");
+    }
+
+    uint blkNumber;
+
+    pte_t *pte = walkpgdirDONTCreate(pgdir,(void *)addr);
+    if(*pte!=0){
+        if( ((*pte) & PTE_SWAPPED) != 0 && ((*pte) & PTE_P) == 0){
+            blkNumber = PTE_ADDR(*pte)>>12;
+            read_page_from_disk(1,allocatedPage,blkNumber);
+            
+            *pte = V2P(allocatedPage) | PTE_FLAGS(*pte);
+
+            *pte |= PTE_P; //Setting Present Bit to 1
+            *pte &= ~PTE_SWAPPED; //Setting SWAPPED Bit to 0
+            bfree_page(1,blkNumber);
+        } else{
+            cprintf("((*pte) & PTE_SWAPPED):%d \n((*pte) & PTE_P):%d\n", ((*pte) & PTE_SWAPPED),((*pte) & PTE_P));
+            panic("map_address is being called on a pte which is !(SWAPPED=1,PTE_P=0)");  
+        } 
+        return;
+    }
+
+    mappages(pgdir,(void *)addr,PGSIZE, V2P(allocatedPage), PTE_W|PTE_U);
 }
 
 /* page fault handler */
